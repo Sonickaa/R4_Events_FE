172ec1777efb227f7bfa582794b620d0a174bf1a
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-11-29 16:31:22 +0100

    ContactForm

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,17 @@ import Footer from "./components/Footer";
 import React from "react";
 import "./App.css";
 import Navbar from "./components/Navbar";
+import { Navigate, Route, Routes } from "react-router-dom";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <>
       <Navbar />
       <HoverColor />
+      <Routes>
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   );
