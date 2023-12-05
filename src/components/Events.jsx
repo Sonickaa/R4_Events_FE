01dd2a949b3d7d8f637840833b7629b4917a8fc5
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-12-05 13:19:28 +0100

    events-width-change

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,30 +1,36 @@
+import "./css/Events.css";
+
 export default function Events() {
   return (
     <>
-      <div style={{ margin: "1em" }}>
+      <div className="event-container">
         <h4 style={{ color: "white" }}>
           Photos by dearest Althea, taken on her Polaroid cameras during our
           events:{" "}
         </h4>
-        <img src="\Scansione 1.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 2.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 5.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 9.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 10.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 11.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 14.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 24.jpg" alt="polaroid-photo" />
-        <img src="\Scansione 55.jpg" alt="polaroid-photo" />
+        <div className="event-images">
+          <img src="\Scansione 1.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 2.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 5.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 9.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 10.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 11.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 14.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 24.jpg" alt="polaroid-photo" />
+          <img src="\Scansione 55.jpg" alt="polaroid-photo" />
+        </div>
         <h4 style={{ color: "white" }}>
           Posters and flyers from our past events, made by dearest Carolina,
           Vince or us:
         </h4>
-        <img src="\fuckoff.jpg" alt="poster" />
-        <img src="\tasse2022.jpg" alt="poster" />
-        <img src="\3x2.jpg" alt="poster" />
-        <img src="\vertical-IG-june.jpg" alt="poster" />
-        <img src="\IMG_1766.JPG" alt="poster" />
-        <img src="\plakat print.jpg" alt="poster" />
+        <div className="event-images">
+          <img src="\fuckoff.jpg" alt="poster" />
+          <img src="\tasse2022.jpg" alt="poster" />
+          <img src="\3x2.jpg" alt="poster" />
+          <img src="\vertical-IG-june.jpg" alt="poster" />
+          <img src="\IMG_1766.JPG" alt="poster" />
+          <img src="\plakat print.jpg" alt="poster" />
+        </div>
       </div>
     </>
   );
