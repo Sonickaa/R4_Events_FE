eeaeb592571624536be9ae4b896109cd1d88ceb0
Sonia Chaboud <sonia.chaboud@gmail.com>
2023-12-14 15:22:04 +0100

    photos

--- src/components/Awareness.jsx
+++ src/components/Awareness.jsx
@@ -1,30 +1,30 @@
+import "./css/Awareness.css";
+
 export default function Awareness() {
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        textAlign: "left",
-        alignItems: "center",
-      }}
-    >
-      {" "}
-      <h4
-        style={{
-          color: "white",
-          width: "70%",
-          margin: "2em",
-        }}
-      >
-        We're an open and tolerant place. <br /> No homophobia, no transphobia,
-        no racism, no sexism, no bodyshaming, no violence. <br /> If you
-        encounter or experience any of these situations during our event, please
-        come to us. <br /> Even if you are not sure, if something felt
-        uncomfortable, don't hesitate to ask us. <br />
-        Consent is sexy. <br /> Communicate clearly, get a clear yes, give a
-        clear no. If somebody doesn't understand these simple words, let us
-        know. <br /> Look after each other and have fun!
-      </h4>
-    </div>
+    <>
+      <div className="awareness-container">
+        <h4 className="awareness-text">
+          We're an open and tolerant place. <br /> No homophobia, no
+          transphobia, no racism, no sexism, no bodyshaming, no violence. <br />{" "}
+          If you encounter or experience any of these situations during our
+          event, please come to us. <br /> Even if you are not sure, if
+          something felt uncomfortable, don't hesitate to ask us. <br />
+          Consent is sexy. <br /> Communicate clearly, get a clear yes, give a
+          clear no. If somebody doesn't understand these simple words, let us
+          know. <br /> Look after each other and have fun!
+        </h4>
+      </div>
+      <div className="video-container">
+        <video /* width="600" height="400" */ controls>
+          <source src="R4-video.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+        <video /* width="600" height="400" */ controls>
+          <source src="r4-video2.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,28 +1,20 @@
 import HoverColor from "./HoverColor";
 import "./css/Home.css";
+import { NavLink } from "react-router-dom";
 
 export default function Home() {
   return (
     <>
       <HoverColor />
-      <div className="video">
-        <video /* width="600" height="400" */ controls>
-          <source src="R4-video.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
-      </div>
 
-      <h1
-        style={{
-          color: "#09e8de",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          margin: "2em",
-        }}
-      >
-        Your sexy, friendly, techno & fetish encounter{" "}
-      </h1>
+      <h1>Your sexy, friendly, techno & fetish encounter </h1>
+      <h3>
+        {" "}
+        <NavLink className="link" to="/Shop">
+          {" "}
+          T-Shirts
+        </NavLink>
+      </h3>
     </>
   );
 }
